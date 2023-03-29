d207f16794baf211457f6d90ed154686fcadd464
migruedav <migruedav@hotmail.com>
2023-03-29 11:22:40 -0600

    update 6 files

---
+++ src/components/WordCloud.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactWordcloud from "react-wordcloud";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
+import words from "./words";
+
+const options = {
+  colors: ['#ff0000', '#e60000', '#cc0000', '#b30000', '#990000', '#800000'],
+  enableTooltip: true,
+  deterministic: true,
+  fontFamily: "impact",
+  fontSizes: [10, 80],
+  fontStyle: "normal",
+  fontWeight: "normal",
+  padding: 1,
+  rotations: 3,
+  rotationAngles: [0, 0],
+  scale: "sqrt",
+  spiral: "archimedean",
+  transitionDuration: 1000
+};
+
+function MyWordCloud() {
+  return (
+    <div>
+      <div className="word-cloud" style={{ height: 500, width: 900, marginTop:100 }}>
+        <ReactWordcloud options={options} words={words} />
+      </div>
+    </div>
+  );
+}
+
+export default MyWordCloud;
--- src/paginas/Keywords.js
+++ src/paginas/Keywords.js
@@ -1,7 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import MyWordCloud from "../components/WordCloud";
 
-export default function Keywords() {
+
+export default function MyPage() {
+  const [dias, setDias] = useState(30);
+  const [data, setData] = useState([]);
+  const [tendencia, setTendencia] = useState("tiempo");
+  const [loading, setLoading] = useState(false);
+
+  // FUNCIONES
+
+  const handleButtonClick = (e, state, setState) => {
+    e.preventDefault();
+    setState(!state);
+  };
+
+  const fetchData = async (dias) => {
+    try {
+      setLoading(true);
+      const url = `https://fastapi-production-b90c.up.railway.app/trends?days=${dias}}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+
+  useEffect(() => {
+    fetchData(dias);
+  }, []);
   return (
-    <div>Keywords</div>
-  )
+    <div className="content">
+        <div className="left-container">
+        <div className="tituloizq">
+          <h1 className="titulo">KEYWORDS</h1>
+          <hr className="divider" />
+        </div>
+
+        <select value={dias} onChange={(e) => setDias(e.target.value)}>
+          <option value="7">7 días</option>
+          <option value="15">15 días</option>
+          <option value="30">30 días</option>
+          <option value="60">60 días</option>
+          <option value="120">120 días</option>
+          <option value="180">180 días</option>
+          <option value="360">360 días</option>
+        </select>
+
+        <button
+          className="fetch-button"
+          onClick={() => fetchData(dias)}
+          disabled={loading}
+          style={{ backgroundColor: loading ? "#222222" : "red" }}
+        >
+          {loading ? "Cargando..." : "Realizar Búsqueda"}
+        </button>
+        </div>
+        <div className="right-container">
+            <MyWordCloud/>
+        </div>
+      </div>
+  );
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<MyPage />, rootElement);
+
+
