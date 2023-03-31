9324f133d1b0f4fd2d0d36050a051828065b1809
migruedav <migruedav@hotmail.com>
2023-03-31 08:08:09 -0600

    update GeoChart.jsx

--- src/components/GeoChart.jsx
+++ src/components/GeoChart.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Chart } from 'react-google-charts';
+import React from "react";
+import { Chart } from "react-google-charts";
 
 function GeoChart(props) {
   const chartData = props.data;
 
   const options = {
-    region: 'MX',
-    resolution: 'provinces',
-    colorAxis: { colors: ['#ff0000', '#e60000', '#cc0000', '#b30000', '#990000', '#800000'] },
+    region: "MX",
+    resolution: "provinces",
+    colorAxis: {
+      colors: [
+        "#ff0000",
+        "#e60000",
+        "#cc0000",
+        "#b30000",
+        "#990000",
+        "#800000",
+      ],
+    },
     tooltip: {
-      isHtml: true // Establece el tooltip como HTML
-  }
+      isHtml: true, // Establece el tooltip como HTML
+    },
   };
 
   return (
